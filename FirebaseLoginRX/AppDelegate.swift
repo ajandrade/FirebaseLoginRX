29f62a4a9d68ed33ec2331a8e44e2cd671f5751e
Amadeu Andrade <amadeu.dev@gmail.com>
2017-10-15 16:46:04 +0100

    Enabled facebook

--- FirebaseLoginRX/AppDelegate.swift
+++ FirebaseLoginRX/AppDelegate.swift
@@ -8,17 +8,19 @@
 
 import UIKit
 import Firebase
+import FacebookCore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
   var window: UIWindow?
 
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     
     // Firebase
     FirebaseApp.configure()
+    // Facebook
+    SDKApplicationDelegate.shared.application(application, didFinishLaunchingWithOptions: launchOptions)
 
     window = UIWindow(frame: UIScreen.main.bounds)
     let rootCoordinator = RootCoordinator(window: window!)
@@ -26,6 +28,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     return true
   }
+  
+  func application(_ application: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any])
+    -> Bool {
+      let facebook = SDKApplicationDelegate.shared.application(application, open: url, options: options)
+      
+      return facebook
+  }
+
 
 }
 
